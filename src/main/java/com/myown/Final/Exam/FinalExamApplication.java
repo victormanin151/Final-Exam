a662d9256e83e8897c32f32e7a614ead4ba78a88
Victor Manin <victormanin151@gmail.com>
2026-02-28 16:07:57 +0200

    initial commit

---
+++ src/main/java/com/myown/Final/Exam/FinalExamApplication.java
@@ -0,0 +1,13 @@
+package com.myown.Final.Exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myown/Final/Exam/FinalExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myown.Final.Exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
