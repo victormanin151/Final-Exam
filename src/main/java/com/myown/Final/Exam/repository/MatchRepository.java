b93b76f64abeffdba872170989888a0d70ea3639
Victor Manin <victormanin151@gmail.com>
2026-03-01 17:55:54 +0200

    added match repo

---
+++ src/main/java/com/myown/Final/Exam/repository/MatchRepository.java
@@ -0,0 +1,9 @@
+package com.myown.Final.Exam.repository;
+
+import com.myown.Final.Exam.model.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Long> {
+}
