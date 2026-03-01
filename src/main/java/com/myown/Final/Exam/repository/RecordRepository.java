439b08d14ec0d1e516f2ead34d52c57e0d4db41c
Victor Manin <victormanin151@gmail.com>
2026-03-01 17:58:47 +0200

    added record repo

---
+++ src/main/java/com/myown/Final/Exam/repository/RecordRepository.java
@@ -0,0 +1,9 @@
+package com.myown.Final.Exam.repository;
+
+import com.myown.Final.Exam.model.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
