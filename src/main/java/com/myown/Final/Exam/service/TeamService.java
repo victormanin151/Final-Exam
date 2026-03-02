c71a9a6cff73064cf398a8fa2b9b727882263fe7
V Manin <victormanin151@gmail.com>
2026-03-02 19:01:32 +0200

    added TeamService with createTeam and getAllTeams

---
+++ src/main/java/com/myown/Final/Exam/service/TeamService.java
@@ -0,0 +1,31 @@
+package com.myown.Final.Exam.service;
+
+import com.myown.Final.Exam.dto.TeamDto;
+import com.myown.Final.Exam.model.Team;
+import com.myown.Final.Exam.repository.TeamRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class TeamService {
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public void createTeam(TeamDto dto) {
+        Team team = dto.toEntity();
+        teamRepository.save(team);
+    }
+
+    @Transactional(readOnly = true)
+    public List<TeamDto> getAllTeams() {
+        return teamRepository.findAll()
+                .stream()
+                .map(TeamDto::fromEntity)
+                .toList();
+    }
+}
