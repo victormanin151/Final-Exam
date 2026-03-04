9e3ce9357b3c827b2f8e19d44dabfca5da5fa903
Victor Manin <victormanin151@gmail.com>
2026-03-05 01:20:07 +0200

    added Order in TeamCsvLoader

--- src/main/java/com/myown/Final/Exam/loader/TeamCsvLoader.java
+++ src/main/java/com/myown/Final/Exam/loader/TeamCsvLoader.java
@@ -3,6 +3,7 @@ package com.myown.Final.Exam.loader;
 import com.myown.Final.Exam.dto.TeamDto;
 import com.myown.Final.Exam.service.TeamService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -13,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Component
+@Order(1)
 public class TeamCsvLoader implements CommandLineRunner {
 
     private final TeamService teamService;
