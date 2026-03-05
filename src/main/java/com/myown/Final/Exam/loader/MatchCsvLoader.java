c2571b113973606817a0973d4777d2a8830ce428
Victor Manin <victormanin151@gmail.com>
2026-03-05 02:24:24 +0200

    added MatchCsvLoader

---
+++ src/main/java/com/myown/Final/Exam/loader/MatchCsvLoader.java
@@ -0,0 +1,88 @@
+package com.myown.Final.Exam.loader;
+
+
+import com.myown.Final.Exam.dto.MatchInputDto;
+import com.myown.Final.Exam.dto.PlayerInputDto;
+import com.myown.Final.Exam.service.MatchService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Component
+@Order(3)
+public class MatchCsvLoader implements CommandLineRunner {
+    private final MatchService matchService;
+
+    public MatchCsvLoader(MatchService matchService){
+        this.matchService = matchService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("MatchCsvLoader starting...");
+        load();
+    }
+
+    public void load() throws IOException {
+        InputStream inputStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream("files/matches.csv");
+
+        if (inputStream == null) {
+            throw new IllegalStateException("matches.csv not found");
+        }
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8)
+        );
+
+        List<MatchInputDto> dtos = reader.lines()
+                .skip(1)
+                .map(line -> {
+                    String[] cols = line.split("\\s*,\\s*");
+                    Long id = Long.parseLong(cols[0].trim());
+                    Long homeTeamId = Long.parseLong(cols[1].trim());
+                    Long awayTeamId = Long.parseLong(cols[2].trim());
+                    LocalDate date = LocalDate.parse(cols[3].trim(), DateTimeFormatter.ofPattern("M/d/yyyy"));
+
+                    String scorePart = cols[4].trim();
+                    boolean wentToPenalties = scorePart.contains("(");
+
+                    int homeScore;
+                    int awayScore;
+                    Integer homePenalty = null;
+                    Integer awayPenalty = null;
+
+                    if (wentToPenalties) {
+                        String[] sides = scorePart.split("-");
+                        String homeSide = sides[0];
+                        String awaySide = sides[1];
+
+                        homeScore = Integer.parseInt(homeSide.substring(0, homeSide.indexOf('(')));
+                        awayScore = Integer.parseInt(awaySide.substring(0, awaySide.indexOf('(')));
+
+                        homePenalty = Integer.parseInt(homeSide.substring(homeSide.indexOf('(') + 1, homeSide.indexOf(')')));
+                        awayPenalty = Integer.parseInt(awaySide.substring(awaySide.indexOf('(') + 1, awaySide.indexOf(')')));
+                    } else {
+                        String[] sides = scorePart.split("-");
+                        homeScore = Integer.parseInt(sides[0]);
+                        awayScore = Integer.parseInt(sides[1]);
+                    }
+
+                    return new MatchInputDto(id, homeTeamId, awayTeamId, date, homeScore, awayScore, wentToPenalties, homePenalty, awayPenalty);
+                })
+                .toList();
+        dtos.forEach(System.out::println);
+
+        matchService.createMatches(dtos);
+    }
+}
