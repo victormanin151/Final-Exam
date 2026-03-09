81163318181607800f6f2518370ae27307ea9118
V Manin <victormanin151@gmail.com>
2026-03-09 15:45:13 +0200

    added searchByName function in repo

--- src/main/java/com/myown/Final/Exam/repository/PlayerRepository.java
+++ src/main/java/com/myown/Final/Exam/repository/PlayerRepository.java
@@ -4,6 +4,9 @@ import com.myown.Final.Exam.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    List<Player> findByFullNameContainingIgnoreCase(String fullName);
 }
