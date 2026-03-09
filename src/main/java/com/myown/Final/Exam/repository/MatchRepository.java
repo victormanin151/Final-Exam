98a417ed31a3ced2eacf522a486d921ac22816b0
V Manin <victormanin151@gmail.com>
2026-03-09 17:28:55 +0200

    added getMatchByPenalties in repo

--- src/main/java/com/myown/Final/Exam/repository/MatchRepository.java
+++ src/main/java/com/myown/Final/Exam/repository/MatchRepository.java
@@ -4,6 +4,9 @@ import com.myown.Final.Exam.model.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    List<Match> findByWentToPenaltiesTrue();
 }
